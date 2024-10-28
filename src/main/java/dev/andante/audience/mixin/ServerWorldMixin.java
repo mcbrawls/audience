08a27928b6dbca23b61d1e3f076d55aa9d9b148f
Daniel Orr <orrdaniel73@gmail.com>
2024-10-28 00:56:03 +0000

    chore: update dependencies

--- src/main/java/dev/andante/audience/mixin/ServerWorldMixin.java
+++ src/main/java/dev/andante/audience/mixin/ServerWorldMixin.java
@@ -23,11 +23,11 @@ import java.util.function.Supplier;
 @SuppressWarnings("AddedMixinMembersNamePattern")
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World implements Audience {
-    private ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
-        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
-    }
-
     @Shadow @Final private MinecraftServer server;
+
+    private ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+    }
 
     @Override
     public PlayerSet getAudiencePlayers() {
